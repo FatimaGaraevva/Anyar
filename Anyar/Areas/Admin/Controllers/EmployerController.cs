443555df50afcac5678cf554780d801924628ea6
sunaykazimbeylii <sunaysk-bp220@code.edu.az>
2025-05-16 19:08:01 +0400

    change

---
+++ Anyar/Areas/Admin/Controllers/EmployerController.cs
@@ -0,0 +1,38 @@
+﻿using Anyar.DAL;
+using Anyar.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anyar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EmployerController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public EmployerController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<GetEmployerVM> employerVM = await _context.Employers.Select(e => new GetEmployerVM
+            {
+                Name = e.Name,
+                Surname = e.Surname,
+                Description = e.Description,
+                Image = e.Image,
+                Prefossion = e.Prefossion,
+            }).ToListAsync();
+
+            return View(employerVM);
+        }
+        public async Task<IActionResult> Create()
+        {
+            
+        }
+    }
+}
--- Anyar/Areas/Admin/Controllers/HomeController.cs
+++ Anyar/Areas/Admin/Controllers/HomeController.cs
@@ -8,27 +8,11 @@ namespace Anyar.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-        private readonly AppDbContext _context;
-        private readonly IWebHostEnvironment _env;
-
-        public HomeController(AppDbContext context,IWebHostEnvironment env)
+ 
+      
+        public IActionResult Index()
         {
-            _context=context;
-            _env=env;    
-
-        }
-        public async Task<IActionResult> Index()
-        {
-           List< GetEmployerVM> employerVM = await _context.Employers.Select(e => new GetEmployerVM
-            {
-                Name = e.Name,
-                Surname = e.Surname,
-                Description = e.Description,
-                Image = e.Image,
-                Prefossion = e.Prefossion,
-            }).ToListAsync();
-            
-            return View(employerVM);
+           return View();
         }
     }
 }
